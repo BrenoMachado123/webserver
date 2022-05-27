287c3ddff208777e7de43219be74ea599c23687d
pulgamecanica <pulgamecanica11@gmail.com>
2022-05-27 15:01:23 +0100

    Linux

--- includes/Config.hpp
+++ includes/Config.hpp
@@ -15,22 +15,36 @@
 #include <fstream>
 
 
-# define AUTOINDEX			1
-# define CGI				2
-# define CGIBIN				3
+# define AUTOINDEX		1
+# define CGI			2
+# define CGIBIN			3
 # define CLIENTMAXBODYSIZE	4
-# define ERRORPAGE			5
+# define ERRORPAGE		5
 # define LIMITMETHODS		6
-# define LISTEN				7
-# define LOCATION			8
-# define ROOT				9
-# define SERVERNAME			10
-# define UPLOAD				11
-# define RETURN_D			12
+# define LISTEN			7
+# define LOCATION		8
+# define ROOT			9
+# define SERVERNAME		10
+# define UPLOAD			11
+# define RETURN_D		12
 
 
 // Config represents the configuration file
 class Config {
+	/* PRIVATE EXCEPTIONS */
+	class	InvalidDirectiveException: public std::exception {
+		public:
+			virtual const char * what() const throw();
+	}	e_invalid_directive;
+
+	class	WrongSyntaxException: public std::exception {
+		public:
+			virtual const char * what() const throw();
+	}	e_wrong_syntax;
+	class	InvalidConfigurationFileException: public std::exception {
+		public:
+			virtual const char * what() const throw();
+	}	e_invalid_configuration_file;
 	// ServerConfig represents a server configuration
 	class ServerConfig {
 		public:
@@ -39,31 +53,38 @@ class Config {
 					int _id;
 					Directive();
 				public:
-					Directive(std::string const & name, std::string const & content);
+					Directive(int);
+					virtual int getId() = 0;
 					virtual ~Directive();
 			};
 			class Location: public Directive {
 				std::vector<Directive> _location_directives;
 				public:
 					Location(std::string const &);
+					virtual int getId();
 			};
 			class ServerName: public Directive {
 				public:
 					std::vector<std::string> _names;
+					virtual int getId();
 			};
 			class ClientMaxBody: public Directive {
 				public:
 					int _max;
+					virtual int getId();
 			};
 			class Root: public Directive {
 				public:
 					int _port;
+					virtual int getId();
 			};
 			class Listen: public Directive {
 				public:
 					int _port;
+					virtual int getId();
 				Listen(std::string const &);
 			};
+
 			// TO DO, IMPLEMENT ALL THE DIRECTIVES LEFT
 
 			//Directive const & getListen();
@@ -80,7 +101,7 @@ class Config {
 	private:
 		std::vector<ServerConfig> _servers;
 		std::ifstream & _config_file;
-		
+
 		Config();
 		Config & operator= (const Config &);
 		Config(const Config &);
@@ -91,23 +112,6 @@ class Config {
 		~Config();
 
 	/* MEMBER FUNCTIONS */
-
-	/* EXCEPTIONS */
-
-	class	InvalidDirectiveException: public std::exception {
-		public:
-			virtual const char * what() const throw();
-	}	e_invalid_directive;
-
-	class	WrongSyntaxException: public std::exception {
-		public:
-			virtual const char * what() const throw();
-	}	e_wrong_syntax;
-
-	class	InvalidConfigurationFileException: public std::exception {
-		public:
-			virtual const char * what() const throw();
-	}	e_invalid_configuration_file;
 
 };
 
